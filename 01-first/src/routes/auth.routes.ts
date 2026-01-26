adcb77a9c519cf65571943f995bfcd875ec620fe
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-01-26 21:44:59 +0530

    refresh token handler done

--- 01-first/src/routes/auth.routes.ts
+++ 01-first/src/routes/auth.routes.ts
@@ -4,10 +4,13 @@ import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 import { login, refreshToken, register, verifyEmail } from "../controllers/auth/auth.controller.js";
 import isAuth from "../middleware/auth.middleware.js";
 
+// instance of express router
 const authRouter = Router();
 
+// register route
 authRouter.post('/register', validate(registerSchema), register);
 
+// login route
 authRouter.post('/login', validate(loginSchema), login);
 
 authRouter.get('/verify-email', isAuth, verifyEmail);
